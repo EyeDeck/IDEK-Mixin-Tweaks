af18b45ca3921c413b43eae2acf7f323988abcf5
EyeDeck <2722970+EyeDeck@users.noreply.github.com>
2023-10-31 22:01:55 -0400

    Hack ChC adjacency cores to stop crashing
    also template setup

--- src/main/java/idek/mixintweaks/Idekmixintweaks.java
+++ src/main/java/idek/mixintweaks/Idekmixintweaks.java
@@ -1,20 +1,20 @@
-package ${package};
+package idek.mixintweaks;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ${mainclass}.MODID, version = ${mainclass}.VERSION)
-public class ${mainclass}
+@Mod(modid = Idekmixintweaks.MODID, version = Idekmixintweaks.VERSION)
+public class Idekmixintweaks
 {
-    public static final String MODID = "${modid}";
+    public static final String MODID = "idekmixintweaks";
     public static final String VERSION = "@VERSION@";
 
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
         // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+        System.out.println("IDEK Mixin Tweaks initialized!");
     }
 }
---
+++ src/main/java/idek/mixintweaks/mixin/AdjCoreFix.java
@@ -0,0 +1,20 @@
+package idek.mixintweaks.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.gui.GuiMainMenu;
+
+@Mixin(targets = "Reika/ChromatiCraft/Base/TileEntity/TileEntityWirelessPowered", remap=false)
+public abstract class AdjCoreFix {
+
+    @Inject(method="calcEfficiency", at=@At("HEAD"), cancellable=true)
+    public void calcEfficiency(CallbackInfo info) {
+        // System.out.println("IDEK Mixin Tweaks calcEfficiency cancelled");
+        info.cancel();
+        return;
+        // this.efficiencyBoost = adjacency.getAdjacentUpgrade(this);
+    }
+}
